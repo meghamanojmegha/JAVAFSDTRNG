d0a4249a5787426909a377966356c5e7a8b471de
meghamanojmegha <66957248+meghamanojmegha@users.noreply.github.com>
2021-08-27 12:05:06 +0530

    Add files via upload

---
+++ Spring-Boot-CRUD/SpringExample2/BooksRepository.java
@@ -0,0 +1,8 @@
+package com.ust.repository;
+import org.springframework.data.repository.CrudRepository;
+
+import com.ust.model.Books;
+
+public interface BooksRepository extends CrudRepository<Books, Integer> {
+
+}
---
+++ Spring-Boot-CRUD/SpringbootExample/SpringBootApplication.java
@@ -0,0 +1,10 @@
+package com.ust;
+import org.springframework.boot.SpringApplication;
+@org.springframework.boot.autoconfigure.SpringBootApplication
+
+public class SpringBootApplication {
+public static void main(String array[])
+{
+	SpringApplication.run(SpringBootApplication.class,array);
+}
+}
